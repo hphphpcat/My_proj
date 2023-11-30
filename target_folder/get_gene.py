f5ceda222d53cdf580c7b6236c90908bf6d9b64d
goto_1600 <656695476@qq.com>
2023-11-30 20:51:00 +0800

    modify

--- target_folder/get_gene.py
+++ target_folder/get_gene.py
@@ -1,21 +1,38 @@
-x=1
-i=1
-while i<=128:
-    i+=i
-    x+=x
-print(x)
+import random
+# # print(len("GATCACAGGTCTATCACCCTATTAACCACTCACGGGAGCTCTCCATGCATTTGGTATTTTCGTCTGGGGG"))
+# # DNA碱基
+dna_bases = "ATCG"
 
-with open("bb/minigraph/test/MT-human.fa", "r") as file:
-    gene_fragments = file.readlines()
+for i in range(20):
+    print(str(i)+'.'+'fa' ,end=' ')
+#  生成1e5个随机的DNA序列
+import random
+sequence_length = 2000 # 每个序列的长度
 
-# 使用join方法将基因片段拼接在一起并去掉换行符
-full_gene_sequence = ''.join(gene_fragments).replace('\n', '')
-print(full_gene_sequence)
+sequences = [[] for i in range(20)]
+for i in range(20):
+        random_sequence = ''.join(random.choice(dna_bases) for _ in range(sequence_length))
+        for j in random_sequence:
+            sequences[i].append(j)
 
-print("----")
-with open("bb/minigraph/test/MT-chimp.fa", "r") as file:
-    gene_fragments = file.readlines()
 
-# 使用join方法将基因片段拼接在一起并去掉换行符
-full_gene_sequence = ''.join(gene_fragments).replace('\n', '')
-print(full_gene_sequence)
+for i in range(1,6):
+    sequences[i] = list(sequences[0])
+    for j in range(5,100):
+        sequences[i][j] = random.choice(dna_bases)
+    for j in range(450,600):
+        sequences[i][j] = random.choice(dna_bases)
+    # for j in range(4050,4505):
+    #     sequences[i][j] = random.choice(dna_bases)
+# 将DNA序列写入fa文件
+name = '>HS#NC_012920.'
+
+for i in range(20):
+    fn = '/home/yqmao/data/My_proj/target_folder/minigraph1/'+str(i)+".fa"
+    with open(fn, "w") as file:
+        file.write(name + str(i) +  "\n")
+        for i, sequence in enumerate(sequences[i], start=1):
+            file.write(sequence )
+            
+            
+
